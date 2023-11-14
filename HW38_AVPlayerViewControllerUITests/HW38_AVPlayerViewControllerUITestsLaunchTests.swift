56d24f52f6983ecc26f8bfc39d9270b01000136e
Daweihao84 <dwsamurai84@gmail.com>
2023-11-14 23:17:21 +0800

    Initial Commit

---
+++ HW38_AVPlayerViewControllerUITests/HW38_AVPlayerViewControllerUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  HW38_AVPlayerViewControllerUITestsLaunchTests.swift
+//  HW38_AVPlayerViewControllerUITests
+//
+//  Created by Dawei Hao on 2023/11/14.
+//
+
+import XCTest
+
+final class HW38_AVPlayerViewControllerUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
